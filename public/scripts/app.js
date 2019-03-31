d7ee87168ae5ba88293db57de88fa69f85683e98
Arjay Jones <robert.jones@arjaycodes.com>
2019-03-30 22:47:21 -0700

    populated Header component with basic HTML; created an Action button component

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,6 @@
-"use strict";
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -6,8 +8,13 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Header = function (_React$component) {
-  _inherits(Header, _React$component);
+var app = {
+  title: 'Indecision',
+  'subTitle': 'Make Up Your Mind With The Help of Math'
+};
+
+var Header = function (_React$Component) {
+  _inherits(Header, _React$Component);
 
   function Header() {
     _classCallCheck(this, Header);
@@ -15,5 +22,56 @@ var Header = function (_React$component) {
     return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
   }
 
+  _createClass(Header, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'h1',
+          null,
+          app.title
+        ),
+        React.createElement(
+          'h4',
+          null,
+          app.subTitle
+        )
+      );
+    }
+  }]);
+
   return Header;
-}(React.component);
+}(React.Component);
+
+var Action = function (_React$Component2) {
+  _inherits(Action, _React$Component2);
+
+  function Action() {
+    _classCallCheck(this, Action);
+
+    return _possibleConstructorReturn(this, (Action.__proto__ || Object.getPrototypeOf(Action)).apply(this, arguments));
+  }
+
+  _createClass(Action, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'button',
+        null,
+        'Choose For Me!'
+      );
+    }
+  }]);
+
+  return Action;
+}(React.Component);
+
+var jsx = React.createElement(
+  'div',
+  null,
+  React.createElement(Header, null),
+  React.createElement(Action, null)
+);
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
-class Header extends React.component {
-
+const app = {
+  title: 'Indecision',
+  'subTitle': 'Make Up Your Mind With The Help of Math'
 }
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{app.title}</h1>
+        <h4>{app.subTitle}</h4>
+      </div>
+    );
+  }
+}
+
+class Action extends React.Component {
+  render() {
+    return <button>Choose For Me!</button>;
+  }
+}
+
+const jsx = (
+  <div>
+    <Header />
+    <Action />
+  </div>
+);
+ReactDOM.render(jsx, document.getElementById('app'));
